6a50d30d9115482fc95b9d745451fafbd0635642
Nick Leon <nicolas7@vt.edu>
2021-06-13 12:35:13 -0400

    add password functionality

--- script.js
+++ script.js
@@ -5,9 +5,24 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
 
+  console.log(password)
   passwordText.value = password;
 
+}
+
+function generatePassword() {
+  let length = window.prompt("How many characters?");
+  let retVal = "";
+  let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+  console.log(length)
+
+  for(let i=0; i < length; i++) {
+    retVal += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+  return retVal;
 }
 
 // Add event listener to generate button
