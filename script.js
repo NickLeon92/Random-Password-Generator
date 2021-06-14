9c23f66ee2c51f5f72cb6959b96161d8ea6fdc23
Nick Leon <nicolas7@vt.edu>
2021-06-13 22:14:56 -0400

    simplify correction prompt logic. add comments. remove console logs

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// this function calls the password generator function
+// stores that result in a variable
+//and writes the result
 function writePassword() {
-  console.log("derp")
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
-
+//this function generates the password by first asking a desired character count between 8 and 128
+//The if-loops sends the user back if invalid input is provided
 function generatePassword() {
   let length = window.prompt("Select a number of characters between 8 and 128");
   if (length < 8 || length > 128) {
-    let test = window.confirm("must be between 8 and 128 characters");
-    if (test == true){
-      return generatePassword()
-    }
-    if (test == false){
-      return "Your Secure Password"
-    }
+    window.alert("number needs to be between 8 and 128")
+    return generatePassword()
+  }
+  if (isNaN(length)){
+    window.alert("you need to type a number")
+    return generatePassword()
   }
 
-  let retVal = "";
-  console.log("derp2")
-  charset = generateCharset();
 
+//This for-loop builds the password by selecting a randomized element from the charset
+//The process repeats and adds a new character to the password until the desired character count is reached
+  let retVal = "";
+  charset = generateCharset();
   for(let i=0; i < length; i++) {
     console.log(length)
     retVal += charset.charAt(Math.floor(Math.random() * charset.length));
@@ -33,7 +35,8 @@ function generatePassword() {
   return retVal;
 }
 
-
+//this function generates the charset by providing a series of confirm boxes to choose between character types
+//if no character sets are selected then the user is sent back to the start of the selection process
 function generateCharset(){
   
   let lowercaseCheck = window.confirm("do you want lowercase characters (cancel for no)")
@@ -44,10 +47,16 @@ function generateCharset(){
  
   let specialCheck = window.confirm("do you want special characters (cancel for no)")
 
+  if(lowercaseCheck==false, uppercaseCheck==false, numberCheck==false, specialCheck==false){
+    window.alert("you must choose at least one character set!!")
+    return generateCharset();
+  }
+
+//the funtion builds an array of booleans (deteremind by the confirm boxes) along with a corresponding array of character sets
+//the for-loop builds the character set by going through each element of the boolean array and adding its character sets if chosen 
   test = [lowercaseCheck, uppercaseCheck, numberCheck, specialCheck]
   chararray = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", "!@#$%^&*()<>?[]{};'"]
   let charset = "";
-
   for (i=0; i<4; i++) {
     if(test[i]==true)
     charset = chararray[i] + charset;
